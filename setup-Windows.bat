3353e81f58ba3c13ffa36735cb6129921310aff4
Lunar <144155299+SyncGit12@users.noreply.github.com>
2024-08-24 16:27:41 -0300

    ...

--- setup-Windows.bat
+++ setup-Windows.bat
@@ -36,6 +36,15 @@ haxelib run lime setup flixel
 haxelib run lime setup
 haxelib install flixel-tools
 haxelib run flixel-tools setup
+haxelib set flixel 5.5.0
+haxelib set flixel-addons 3.2.1
+haxelib set lime 8.0.1
+haxelib set openfl 9.3.2
+haxelib set tjson 1.4.0
+haxelib git SScript https://github.com/SScript-Guy/SScript-new
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
 echo Done!
 pause
 exit
